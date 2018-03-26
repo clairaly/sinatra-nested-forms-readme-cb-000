456ea87303a6fe5c97befb6eea019b5d1e89d175
“Alyssa <clairaly13@gmail.comgit config --global user.name clairaly>
2018-03-26 17:01:45 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
-require_relative 'config/environment'
+require './environment'
 
-class App < Sinatra::Base
+module FormsLab
+  class App < Sinatra::Base
 
+    get '/' do
+      erb :'pirates/index'
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      @ship1 = Ship.new(params[:pirate][:ships][0])
+      @ship2 = Ship.new(params[:pirate][:ships][1])
+      puts params
+      erb :'pirates/show'
+    end
+
+  end
 end
